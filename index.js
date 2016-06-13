0ba99e72bd53c7f65d635d6807d52d412073b003
Luca Capra <luca.capra@gmail.com>
2016-06-13 17:46:50 +0200

    initial setup for dbus introspection

--- index.js
+++ index.js
@@ -4,24 +4,39 @@ var d = require("debug")("agile:http");
 
 var express = require("express");
 var bodyParser = require("body-parser");
-var app = express();
 
+var dbus = require("./lib/dbus");
 
-var startServer = function() {
+var startServer = function(app) {
+
+  app = app || express();
 
   app.use(bodyParser.json());
 
-  // TODO implement authentication
+  // TODO implement token based authentication here (?)
   app.use(function(req, res, next) {
 
-    // if(!req.headers.Authorization) {
-    //   return res.sendStatus(401);
-    // }
+    if(!req.headers.Authorization ||
+        req.headers.Authorization.indexOf('Bearer') === -1) {
+      return res.sendStatus(401);
+    }
 
     next();
   });
 
-  // /api/DeviceManager/Load
+  /**
+   * Translates HTTP call to DBUS
+   *
+   * Patterns:
+   *  - Call a function
+   *      `GET MyObject/Load
+   *  - Call a function with arguments
+   *      `POST MyObject/Load { "arg1": "foo", "arg2": "bar" }`
+   *  - Read a property
+   *      `GET MyObject/Devices
+   *  - Write to a property
+   *      `PUT DeviceManager/Devices `raw value`
+   */
   app.all(config.basePath + '/(*)/(*)', function(req, res) {
 
     var method = req.method;
@@ -33,8 +48,13 @@ var startServer = function() {
   app.listen(config.port, config.host, function() {
     d("Started API at http://%s:%s%s", config.host, config.port, config.basePath);
   });
+
+  return app;
 };
 
 module.exports.start = function() {
+
+  dbus.loadObjects();
+
   startServer();
 };
